d54641832a8f610ff84886332d7d42a7dff85ab8
yannickLega <yannick.lega@yahoo.com>
2022-05-19 16:33:30 +0200

    add review controller create + update +delete + update productRating

--- api/review/controllers/review.js
+++ api/review/controllers/review.js
@@ -1,8 +1,70 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.review.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.review.create(ctx.request.body);
+    }
+
+    await strapi.services.review.average(entity.product.id);
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [review] = await strapi.services.review.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!review) {
+      return ctx.unauthorized("You can't update this entry");
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.review.update(data, { files });
+    } else {
+      entity = await strapi.services.review.update(
+        { id },
+        ctx.request.body
+      );
+    }
+
+    await strapi.services.review.average(entity.product.id);
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    const [review] = await strapi.services.review.find({
+      id,
+      user: ctx.state.user.id,
+    });
+
+    if (!review) {
+      return ctx.unauthorized("You can't delete this entry");
+    }
+
+    const entity = await strapi.services.review.delete({ id });
+    await strapi.services.review.average(entity.product.id);
+    return sanitizeEntity(entity, { model: strapi.models.review });
+  },
+};
